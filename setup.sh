d3c6b4b7337ad0568d0b8542b96560c9cd413c2a
Steven D. Shelton <steven.d.shelton@gmail.com>
2018-07-23 12:32:50 -0400

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -3,22 +3,34 @@
 # Update packages
 echo "Running yum update..."
 yum -y update
-echo "installing PHP 7.X"
+echo "installing PHP 7 ..."
 yum -y install epel-release yum-utils
 yum -y install http://rpms.remirepo.net/enterprise/remi-release-7.rpm
 yum-config-manager --enable remi-php72
 yum -y install php php-common php-opcache php-mcrypt php-cli php-gd php-curl php-mysql
-echo "Install Apache"
+echo "Installing Apache ..."
 yum -y install httpd
 echo "Start Apache"
 systemctl start httpd
+echo "installing MariaDB ..."
+yum -y install mariadb-server
+systemctl start mariadb
+systemctl status mariadb
+mysql -u root -e "create database LuptonDB";
+mysql -u root LuptonDB < /vagrant/LuptonDB.sql;
+mysql> GRANT ALL PRIVILEGES ON LuptonDB.* TO 'root'@'localhost';
+echo "creating default db connection file ..."
+mkdir -p /var/www/private
+echo -ne "[database]\nservername = localhost\nusername = root\npassword = \ndbname = LuptonDB\n" > config.ini
+cp config.ini /var/www/private/config.ini
+echo "installing git ..."
 yum -y install git
 echo "Let's get our git files"
 if [ -z "$(ls -A /var/www/html/)" ]; then
    git clone https://github.com/utclibrary/liblab.git /var/www/html/
 else
    cd /var/www/html/
-git pull
+   git pull
 cd ~/
 fi
 
