8f34427e746e8055284b3cfff94d5183f64a18f7
Steven D. Shelton <Steven-Shelton@UTC.edu>
2019-04-10 09:59:48 -0400

    modified setup to force PHP error reporting on

--- setup.sh
+++ setup.sh
@@ -12,6 +12,8 @@ yum-config-manager --enable remi-php72
 yum -y install php php-common php-opcache php-mcrypt php-cli php-gd php-curl php-mysql
 echo "Installing Apache ..."
 yum -y install httpd
+echo "Modify php.ini to show php errors"
+echo "display_errors = On" >> /etc/php.ini
 echo "Start Apache"
 systemctl start httpd
 echo "Installing MariaDB ..."
@@ -32,15 +34,15 @@ mkdir -p /var/www/private
 echo -ne "[database]\nservername = localhost\nusername = root\npassword = \n" > config.ini
 cp config.ini /var/www/private/config.ini
 echo "Installing git ..."
-yum -y install git
-echo "Getting our git files"
-if [ -z "$(ls -A /var/www/html/)" ]; then
-   git clone https://github.com/utclibrary/liblab.git /var/www/html/
-else
-   cd /var/www/html/
-   git pull
-cd ~/
-fi
+#yum -y install git
+#echo "Getting our git files"
+#if [ -z "$(ls -A /var/www/html/)" ]; then
+#   git clone https://github.com/utclibrary/liblab.git /var/www/html/
+#else
+   #cd /var/www/html/
+   #git pull
+#cd ~/
+#fi
 
 setenforce 0
 echo "use 192.168.33.10 OR localhost:8080"
